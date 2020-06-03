62c471538a1397de5fa4694c9eceb638946decf1
Damian Perera <damian.perera@sysco.com>
2020-06-03 20:51:09 +0530

    Updates

---
+++ precache-manifest.27cb81b5c4aa1527564678636fb7bc8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c052bee808597be1de09160588e785",
+    "url": "/twitter-trends/index.html"
+  },
+  {
+    "revision": "a4d8bed9f3e27f2402be",
+    "url": "/twitter-trends/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea9cf3e2747c0de682e0",
+    "url": "/twitter-trends/static/js/2.e1d8f992.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twitter-trends/static/js/2.e1d8f992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d8bed9f3e27f2402be",
+    "url": "/twitter-trends/static/js/main.58b01577.chunk.js"
+  },
+  {
+    "revision": "ef6dcf3605a364f75ec3",
+    "url": "/twitter-trends/static/js/runtime-main.f0c7e573.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitter-trends/static/media/logo.5d5d9eef.svg"
+  }
+]);
