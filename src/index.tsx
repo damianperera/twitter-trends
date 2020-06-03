d0aa4770df4db8c30b0b3374d280576555f66ea2
Damian Perera <damian.perera@sysco.com>
2020-06-03 21:04:32 +0530

    Added root route

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
