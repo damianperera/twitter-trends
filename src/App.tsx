989c0d3a757452c4334f801e8e03d16d7f824803
Damian Perera <damian.perera@sysco.com>
2020-06-04 09:29:53 +0530

    Added console log for environment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const REACT_APP_MODE = process.env.REACT_APP_MODE
+
+  if (REACT_APP_MODE === 'Development') {
+    console.log('This is a development')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
